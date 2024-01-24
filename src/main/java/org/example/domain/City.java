521d648f4b1d813633a3584964b08ef1ecc7c9dd
Mary <p@>
2024-01-24 05:21:04 +0300

    ,

---
+++ src/main/java/org/example/domain/City.java
@@ -0,0 +1,28 @@
+package org.example.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Entity
+@Table(schema = "world", name = "city")
+public class City {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "country_id")
+    private Country country;
+
+    private String district;
+
+    private Integer population;
+
+
+}
